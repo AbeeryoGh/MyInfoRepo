a42edd419c1b167a2519f37fee648ddd7a0a4674
DESKTOP-20DNSTB\96393 <mohannad.mostafa@infostrategic.com>
2022-01-22 11:17:47 +0200

    Initial project training.

---
+++ trainingDotNetCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace trainingDotNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
