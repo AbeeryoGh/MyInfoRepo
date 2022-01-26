2cdd13facbb3004e60ecfb1e2ba0737c0069109c
abeer <abeeryousef91@outlook.com>
2022-01-26 15:48:54 +0200

    Adding ApplicationDbContext to Repository Layer

---
+++ EmpRepositoryLayer/ApplicationDbContext.cs
@@ -0,0 +1,50 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+using EmpDomainLayer.EntityMapper;
+using EmpDomainLayer.Models;
+
+namespace EmpRepositoryLayer
+{
+  public class ApplicationDbContext : DbContext
+  {
+
+    public ApplicationDbContext()
+    {
+    }
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public virtual DbSet<Employee> Employee { get; set; }
+    public virtual DbSet<ETask> Task { get; set; }
+    public virtual DbSet<Vacation> Vacation { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      if (!optionsBuilder.IsConfigured)
+      {
+        optionsBuilder.UseSqlServer("Name=EmpDBCon");
+      }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+      base.OnModelCreating(modelBuilder);
+      modelBuilder.ApplyConfiguration(new EmployeeMap());
+      modelBuilder.ApplyConfiguration(new TaskMap());
+      modelBuilder.ApplyConfiguration(new VacationMap());
+
+      //  modelBuilder.Entity<Employee>()
+      //   .HasMany(c => c.Tasks)
+      //   .WithOne(e => e.Employee);
+
+      //  modelBuilder.Entity<Employee>()
+      //.HasMany(c => c.Vacations)
+      //.WithOne(e => e.Employee);
+
+
+    }
+
+  }
+}
