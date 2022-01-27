9c2a8728da5f03af3073576156aa91907f964702
abeer <abeeryousef91@outlook.com>
2022-01-27 16:56:14 +0200

    Versioning api

--- EmpWebAPI/Controllers/VacationsController.cs
+++ EmpWebAPI/Controllers/VacationsController.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace EmpWebAPI.Controllers
 {
-  [Route("api/[controller]")]
+  [Route("api/v1/[controller]")]
   [ApiController]
   public class VacationController : ControllerBase
   {
